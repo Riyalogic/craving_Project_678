ea2dd1713e4d342a213c485de89ca837412b2aec
Riya Jogi <riyajogi7@gmail.com>
2026-07-16 22:22:48 +0530

    feat: add RestaurantDashboard and RiderDashboard components with role-based access control and navigation; implement sidebar and tab functionality

--- client/src/pages/dashboard/AdminDashboard.jsx
+++ client/src/pages/dashboard/AdminDashboard.jsx
@@ -6,3 +6,17 @@ import AdminOverview from "../../component/adminDashboard/AdminOverview"
 import AdminSetting from "../../component/adminDashboard/AdminSetting";
 import AdminSidebar from "../../component/adminDashboard/AdminSidebar";
 
+const AdminDashboard = () => {
+    const {isLogin, role} = useAuth();
+    const navigate = useNavigate();
+    const active = useLocation().state?.activeTab;
+    const [activeTab, setActiveTab] = React.useState(active || "overview");
+
+    if (!isLogin || role !== "admin") {
+        return(
+            <div className=" h-[92vh] bg-[-url('/foodTable.webp)] bg-cover bg-center">
+                <div className=" h-full backdrop-blur-lg flex flex-col items-center"></div>
+            </div>
+        )
+    }
+}
